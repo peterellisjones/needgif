251512f01f4d50781abca6d1b1733ddaae206a0d
Peter Ellis Jones <pj@ukoki.com>
2013-05-07 23:13:30 +0800

    added new relic

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'newrelic_rpm'
 require 'uri'
 require 'nokogiri'
 require 'open-uri'
@@ -9,7 +10,6 @@ class App < Sinatra::Application
 
   get '/:query' do
     query = params['query']
-    halt(400, "should be of format: gifme.ukoki.com/funny-cat.gif") unless query
 
     # remove filetype
     query = query.gsub(/\.[a-zA-Z]+$/, "")
@@ -19,13 +19,17 @@ class App < Sinatra::Application
 
     url = "https://www.google.com/search?hl=en&tbm=isch&tbs=ift:gif,itp:animated&q=#{query}"
 
-    # begin
-    #   html = Nokogiri::HTML(open(url))
-    #   random_image_url = html.css('.images_table td a').to_a.sample[:href]
-    #   original_image_url = CGI::parse(random_image_url.gsub(/^[^\?]+\?/, ''))["imgurl"][0]
-    #   redirect original_image_url
-    # rescue Exception => e
+    begin
+      html = Nokogiri::HTML(open(url))
+      random_image_url = html.css('.images_table td a').to_a.sample[:href]
+      original_image_url = CGI::parse(random_image_url.gsub(/^[^\?]+\?/, ''))["imgurl"][0]
+      redirect original_image_url
+    rescue Exception => e
       redirect "/assets/not_found.gif"
-    # end
+    end
+  end
+
+  get '/' do
+    redirect "/assets/not_found.gif"
   end
 end
