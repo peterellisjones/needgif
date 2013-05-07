0128f3b9a25169a3e12208053272162e07230a07
Peter Ellis Jones <pj@ukoki.com>
2013-05-07 23:31:11 +0800

    added memcache

--- app.rb
+++ app.rb
@@ -4,9 +4,15 @@ require 'uri'
 require 'nokogiri'
 require 'open-uri'
 require 'cgi'
+require 'json'
+use Rack::Logger
 
 class App < Sinatra::Application
-  enable :raise_errors
+  enable :raise_errors, :logging
+
+  get '/favicon.ico' do
+    redirect "/assets/not_found.gif"
+  end
 
   get '/:query' do
     query = params['query']
@@ -19,11 +25,24 @@ class App < Sinatra::Application
 
     url = "https://www.google.com/search?hl=en&tbm=isch&tbs=ift:gif,itp:animated&q=#{query}"
 
+    # check memcache
+    m_query = "QUERY:"+query.to_s
+    begin
+      ret = $Cache.get(m_query)
+      (redirect(image_url) and return) if image_url = JSON.parse(ret).sample
+    rescue
+    end
+
+    # otherwise grab from google and put in cache
     begin
       html = Nokogiri::HTML(open(url))
-      random_image_url = html.css('.images_table td a').to_a.sample[:href]
-      original_image_url = CGI::parse(random_image_url.gsub(/^[^\?]+\?/, ''))["imgurl"][0]
-      redirect original_image_url
+      image_urls = []
+      html.css('.images_table td a').each do |a|
+        image_urls.push CGI::parse(a[:href].gsub(/^[^\?]+\?/, ''))["imgurl"][0]
+      end
+      (redirect("/assets/not_found.gif") and return) if image_urls.length == 0
+      $Cache.set(m_query, image_urls.to_json)
+      redirect image_urls.sample
     rescue Exception => e
       redirect "/assets/not_found.gif"
     end
--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,16 @@
 require 'sinatra'
 require 'yaml'
+require 'dalli'
+require 'memcachier'
 
 # load config file
 config = YAML::load(File.open('./config/settings.yml'))
 config[settings.environment.to_s].each do |k, v|
   set k.to_sym, v
 end
+
+# load memcache client
+
+$Cache = Dalli::Client.new
 
 require './app'
